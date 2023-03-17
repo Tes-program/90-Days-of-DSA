87bc074b1d4772ab7f315e9fe2116c7a7a1234ad
Grandbusta <bustajay30@gmail.com>
2023-03-17 14:26:57 +0100

    reverse integer solution done

--- ReverseInteger/reverseinteger.go
+++ ReverseInteger/reverseinteger.go
@@ -1,14 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // Link: https://leetcode.com/problems/reverse-integer/
 // Run: go run ReverseInteger/reverseinteger.go
 
 func reverse(x int) int {
-	return x
+	rev := 0
+	for x != 0 {
+		rev = rev*10 + x%10
+		x = x / 10
+	}
+	if rev > math.MaxInt32 || rev < math.MinInt32 {
+		return 0
+	}
+	return rev
 }
-
 func main() {
-	fmt.Println(reverse(123))
+	fmt.Println(reverse(123))  // 321
+	fmt.Println(reverse(-123)) // -321
+	fmt.Println(reverse(120))  // -21
 }
