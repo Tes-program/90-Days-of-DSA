19462dc0f6ebf36d444fd07fc68c3f908c52597d
Grandbusta <bustajay30@gmail.com>
2023-03-17 13:41:39 +0100

    two sum solution

---
+++ ReverseInteger/reverseinteger.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Link: https://leetcode.com/problems/reverse-integer/
+// Run: go run ReverseInteger/reverseinteger.go
+
+func reverse(x int) int {
+	return x
+}
+
+func main() {
+	fmt.Println(reverse(123))
+}
---
+++ TwoSum/twosum.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Link: https://leetcode.com/problems/two-sum
+// Run: go run TwoSum/twosum.go
+
+func twoSum(nums []int, target int) []int {
+	hash := make(map[int]int, 0)
+	result := make([]int, 0)
+	for i, v := range nums {
+		second := target - v
+		if _, ok := hash[second]; !ok {
+			hash[v] = i
+		} else {
+			result = append(result, hash[second], i)
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9)) // [0,1]
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))      // [1,2]
+	fmt.Println(twoSum([]int{3, 3}, 6))         // [0,1]
+}
