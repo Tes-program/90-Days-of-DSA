56db0a761c96beab9f8ca5b7569722c48ba5982e
d-lite <olabemiwodaniel@gmail.com>
2023-03-17 20:24:12 +0100

    day2: reverse integer

---
+++ 2/reverseinteger.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int rev = 0;
+
+        while (x != 0)
+        {
+            if (abs(rev) > 214748364)
+                return 0;
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev;
+    }
+};
+
+// https://leetcode.com/problems/reverse-integer/submissions/917053116/
