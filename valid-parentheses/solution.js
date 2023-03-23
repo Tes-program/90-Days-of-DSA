bbbfd304571a62a9b65316f0cad9d025953cb2cc
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2023-03-23 22:49:49 +0100

    Added more solutions

---
+++ valid-parentheses/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const openingBrackets = ["(", "{", "[" ]
+    const closingBrackets = [")", "}", "]"]
+    let arr = []
+    
+    let isClosing = false
+    
+    if (s.length === 1) return false
+    for (let i = 0; i < s.length; i++){
+        if (closingBrackets.includes(s[0])) return false;
+        
+        if (openingBrackets.includes(s[i])){
+            arr.push(openingBrackets.indexOf(s[i]))
+        }
+        if (closingBrackets.includes(s[i])){
+            isClosing = true
+            let x = closingBrackets.indexOf(s[i])
+            let y = arr.pop()
+            
+            if (x !== y) return false;
+        }
+    }
+    return true && isClosing && arr.length==0
+};
